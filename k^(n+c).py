d11f1aa97e889daa164c35c9fa4bc946eded4819
Weragin <109145593+Weragin@users.noreply.github.com>
2022-12-21 09:05:48 +0100

    Add files via upload

---
+++ k^(n+c).py
@@ -0,0 +1,21 @@
+import numpy
+
+
+def Lines(k:int, n:int, c:int = -1) -> int:
+    """
+    Counts the lines of a k^n game with c cyclic dimensions. Surely working only for.
+    :param k: width of the game board
+    :param n: number of dimensions
+    :param c: number of cyclic dimensions
+    :return: number of Lines
+    """
+
+
+def drawable(k: int, n:int, c:int = -1) -> bool:
+    """
+    Returns True, if a k^n game with c cyclic dimensions can end in draw.
+    :param k: width of the game board
+    :param n: number of dimensions
+    :param c: number of cyclic dimensions
+    :return: The board can end in draw
+    """
